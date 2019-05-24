7d19f086172fd455f6925998ac293251d505b6cb
David Cumps <david@cumps.be>
2019-05-24 23:41:42 +0200

    feat: add parcel

---
+++ applications/basisregisters/grar/parcel/ecs-api.tf
@@ -0,0 +1,104 @@
+resource "aws_service_discovery_service" "api" {
+  name = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-parcel-registry-api"
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+
+  dns_config {
+    namespace_id = "${var.disco_namespace_id}"
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+}
+
+resource "aws_ecs_service" "api" {
+  name            = "${var.app}-parcel-registry-api"
+  cluster         = "${var.fargate_cluster_id}"
+  launch_type     = "FARGATE"
+  task_definition = "${aws_ecs_task_definition.api.arn}"
+  desired_count   = "${var.api_replicas}"
+
+  network_configuration {
+    security_groups = ["${var.task_security_group_id}"]
+    subnets         = ["${var.private_subnets}"]
+  }
+
+  service_registries {
+    registry_arn = "${aws_service_discovery_service.api.arn}"
+  }
+
+  // ordered_placement_strategy {
+  //   type   = "spread"
+  //   field  = "attribute:ecs.availability-zone"
+  // }
+
+  // ordered_placement_strategy {
+  //   type   = "spread"
+  //   field  = "instanceId"
+  // }
+
+  # [after initial apply] don't override changes made to task_definition
+  # from outside of terraform (i.e.; fargate cli)
+  // lifecycle {
+  //   ignore_changes = ["task_definition"]
+  // }
+}
+
+resource "aws_ecs_task_definition" "api" {
+  family                   = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-parcel-registry-api"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "${var.api_cpu}"
+  memory                   = "${var.api_memory}"
+  execution_role_arn       = "${var.task_execution_role_arn}"
+
+  // task_role_arn         = "${aws_iam_role.app_role.arn}"
+  container_definitions = "${data.template_file.api.rendered}"
+
+  tags {
+    Name        = "Parcel Registry Api // ${var.environment_label} ${var.environment_name}"
+    Environment = "${var.tag_environment}"
+    Productcode = "${var.tag_product}"
+    Programma   = "${var.tag_program}"
+    Contact     = "${var.tag_contact}"
+  }
+}
+
+data "template_file" "api" {
+  template = "${file("${path.module}/ecs-api.json.tpl")}"
+
+  vars {
+    environment_name  = "${lower(replace(var.environment_name, " ", "-"))}"
+    datadog_api_key   = "${var.datadog_api_key}"
+    disco_namespace   = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}"
+    app_name          = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-parcel-registry-api"
+    logging_name      = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-parcel-registry"
+    import_api_image  = "${var.import_api_image}"
+    legacy_api_image  = "${var.legacy_api_image}"
+    extract_api_image = "${var.extract_api_image}"
+    import_port       = "${var.port_range}"
+    legacy_port       = "${var.port_range + 2}"
+    extract_port      = "${var.port_range + 4}"
+    region            = "${var.region}"
+    datadog_env       = "${var.datadog_env}"
+    tag_environment   = "${var.tag_environment}"
+    tag_product       = "${var.tag_product}"
+    tag_program       = "${var.tag_program}"
+    tag_contact       = "${var.tag_contact}"
+
+    public_zone_name  = "${replace(var.public_zone_name, "/[.]$/", "")}"
+    // private_zone_name = "${replace(var.private_zone_name, "/[.]$/", "")}"
+    // disco_zone_name   = "${replace(var.disco_zone_name, "/[.]$/", "")}"
+
+    db_server = "${var.db_server}"
+    db_name   = "${var.db_name}"
+    db_user   = "${var.db_user}"
+    db_pass   = "${var.db_password}"
+  }
+}
---
+++ applications/basisregisters/root/parcel.tf
@@ -0,0 +1,51 @@
+variable "parcel_password" {}
+
+module "parcel-registry" {
+  source = "../grar/parcel"
+
+  region            = "${var.aws_region}"
+  environment_label = "${var.environment_label}"
+  environment_name  = "${var.environment_name}"
+
+  tag_environment = "${var.tag_environment}"
+  tag_product     = "${var.tag_product}"
+  tag_program     = "${var.tag_program}"
+  tag_contact     = "${var.tag_contact}"
+
+  app        = "basisregisters"
+  port_range = 7000
+
+  api_cpu           = 256
+  api_memory        = 512
+  api_replicas      = 2
+  legacy_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/parcel-registry/api-legacy:1.4.0"
+  import_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/parcel-registry/api-crab-import:1.4.0"
+  extract_api_image = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/parcel-registry/api-extract:1.4.0"
+
+  projections_cpu      = 256
+  projections_memory   = 512
+  projections_replicas = 1
+  projections_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/parcel-registry/projector:1.4.0"
+  syndication_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/parcel-registry/projections-syndication:1.4.0"
+
+  db_server = "${data.terraform_remote_state.sqlserver.parcel}"
+  sa_user  = "${var.sql_username}"
+  sa_pass  = "${var.sql_password}"
+  db_password = "${var.parcel_password}"
+
+  task_execution_role_arn = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  task_security_group_id  = "${module.public-api.task_security_group_id}"
+
+  vpc_id          = "${data.terraform_remote_state.vpc.vpc_id}"
+  private_subnets = ["${data.terraform_remote_state.vpc.private_subnet_ids}"]
+
+  disco_namespace_id = "${aws_service_discovery_private_dns_namespace.basisregisters.id}"
+  public_zone_name   = "${data.terraform_remote_state.dns.public_zone_name}"
+  disco_zone_name    = "${var.disco_zone_name}"
+
+  datadog_api_key        = "${data.terraform_remote_state.datadog.datadog_api_key}"
+  datadog_logging_lambda = "${data.terraform_remote_state.datadog.datadog_lambda_arn}"
+  datadog_env            = "vbr-${lower(var.environment_name)}"
+
+  fargate_cluster_id = "${data.terraform_remote_state.fargate.fargate_cluster_id}"
+}
