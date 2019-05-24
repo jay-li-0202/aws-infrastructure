9ee5fb6b63bac87ce69dad6bd045704be6446e88
David Cumps <david@cumps.be>
2019-05-24 23:26:51 +0200

    feat: add streetname

---
+++ applications/basisregisters/grar/streetname/sql.tf
@@ -0,0 +1,20 @@
+data "template_file" "sql" {
+  template = "${file("${path.module}/../registry.sql")}"
+
+  vars {
+    database = "${var.db_name}"
+    registry = "StreetnameRegistry"
+    user     = "${var.db_user}"
+    password = "${var.db_password}"
+  }
+}
+
+resource "null_resource" "db_setup" {
+  triggers {
+    key = "${uuid()}"
+  }
+
+  provisioner "local-exec" {
+    command = "sqlcmd -S tcp:127.0.0.1,9001 -U ${var.sa_user} -P ${var.sa_pass} -q \"${data.template_file.sql.rendered}\""
+  }
+}
