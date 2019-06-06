8e4acf7a09101981f62184ec92cf27495b320d24
David Cumps <david@cumps.be>
2019-06-06 13:56:08 +0200

    feat: add import schema

--- applications/basisregisters/grar/registry.sql
+++ applications/basisregisters/grar/registry.sql
@@ -8,6 +8,8 @@ USE [${database}]
 GO
 IF SCHEMA_ID(N'${registry}') IS NULL EXEC(N'CREATE SCHEMA [${registry}];');
 GO
+IF SCHEMA_ID(N'${registry}Import') IS NULL EXEC(N'CREATE SCHEMA [${registry}Import];');
+GO
 IF SCHEMA_ID(N'${registry}Legacy') IS NULL EXEC(N'CREATE SCHEMA [${registry}Legacy];');
 GO
 IF SCHEMA_ID(N'${registry}Extract') IS NULL EXEC(N'CREATE SCHEMA [${registry}Extract];');
