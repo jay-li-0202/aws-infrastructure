956df47326c9e17fd474b2156f6963306af44d06
David Cumps <david@cumps.be>
2019-06-27 14:22:42 +0200

    feat: postal 1.6.1

--- applications/basisregisters/root/postal.tf
+++ applications/basisregisters/root/postal.tf
@@ -15,21 +15,21 @@ module "postal-registry" {
   app        = "basisregisters"
   port_range = 3000
 
-  api_cpu           = 256
-  api_memory        = 512
+  api_cpu           = 512
+  api_memory        = 1024
   api_replicas      = 2
-  legacy_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-legacy:1.5.1"
-  import_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-crab-import:1.5.1"
-  extract_api_image = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-extract:1.5.1"
+  legacy_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-legacy:1.6.1"
+  import_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-crab-import:1.6.1"
+  extract_api_image = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/api-extract:1.6.1"
 
-  projections_cpu      = 256
-  projections_memory   = 512
+  projections_cpu      = 512
+  projections_memory   = 1024
   projections_replicas = 1
-  projections_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/projector:1.5.1"
-  syndication_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/projections-syndication:1.5.1"
+  projections_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/projector:1.6.1"
+  syndication_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/postal-registry/projections-syndication:1.6.1"
 
-  cache_cpu    = 256
-  cache_memory = 512
+  cache_cpu    = 512
+  cache_memory = 1024
   cache_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/redis/redis-populator:1.3.0"
   cache_server = "${data.terraform_remote_state.cache.cache_endpoint}"
 
