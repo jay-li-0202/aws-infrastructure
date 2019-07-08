9f122f0b4d3ae6de709e775477306f730d256df3
David Cumps <david@cumps.be>
2019-07-08 14:17:13 +0200

    feat: add addressregistry

--- applications/basisregisters/root/address.tf
+++ applications/basisregisters/root/address.tf
@@ -15,28 +15,28 @@ module "address-registry" {
   app        = "basisregisters"
   port_range = 5000
 
-  api_cpu           = 256
-  api_memory        = 512
+  api_cpu           = 512
+  api_memory        = 1024
   api_replicas      = 2
-  legacy_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-legacy:1.4.0"
-  import_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-crab-import:1.4.0"
-  extract_api_image = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-extract:1.4.0"
+  legacy_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-legacy:1.5.1"
+  import_api_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-crab-import:1.5.1"
+  extract_api_image = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/api-extract:1.5.1"
 
-  projections_cpu      = 256
-  projections_memory   = 512
+  projections_cpu      = 512
+  projections_memory   = 1024
   projections_replicas = 1
-  projections_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/projector:1.4.0"
-  syndication_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/projections-syndication:1.4.0"
+  projections_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/projector:1.5.1"
+  syndication_image    = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/address-registry/projections-syndication:1.5.1"
 
-  cache_cpu    = 256
-  cache_memory = 512
+  cache_cpu    = 512
+  cache_memory = 1024
   cache_image  = "${var.aws_account_id}.dkr.ecr.eu-west-1.amazonaws.com/redis/redis-populator:1.3.0"
   cache_server = "${data.terraform_remote_state.cache.cache_endpoint}"
 
   db_server   = "${data.terraform_remote_state.sqlserver.address}"
   sa_user     = "${var.sql_username}"
   sa_pass     = "${var.sql_password}"
-  db_password = "${var.address_password}"
+  db_password = "${var.streetname_password}"
 
   ops_lb_arn          = "${module.ops-api.lb_arn}"
   ops_lb_listener_arn = "${module.ops-api.lb_listener_arn}"
