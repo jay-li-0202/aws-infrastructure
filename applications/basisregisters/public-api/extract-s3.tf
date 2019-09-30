b37c024251fe04724e17a939b7bb258c380a062c
David Cumps <david@cumps.be>
2019-09-30 18:34:21 +0200

    feat: add extracts logging bucket

--- applications/basisregisters/public-api/extract-s3.tf
+++ applications/basisregisters/public-api/extract-s3.tf
@@ -1,3 +1,36 @@
+resource "aws_s3_bucket" "extract-logs" {
+  bucket        = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-extracts-logs"
+  acl           = "log-delivery-write"
+  force_destroy = true
+
+  tags = {
+    Name        = "Extracts Logs // ${var.environment_label} ${var.environment_name}"
+    Environment = var.tag_environment
+    Productcode = var.tag_product
+    Programma   = var.tag_program
+    Contact     = var.tag_contact
+  }
+
+  lifecycle_rule {
+    id                                     = "cleanup"
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 1
+    prefix                                 = ""
+
+    expiration {
+      days = var.extracts_expiration_days
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
 resource "aws_s3_bucket" "extract" {
   bucket        = "${var.app}-${lower(replace(var.environment_name, " ", "-"))}-extracts"
   acl           = "public-read"
@@ -9,6 +42,11 @@ resource "aws_s3_bucket" "extract" {
     Productcode = var.tag_product
     Programma   = var.tag_program
     Contact     = var.tag_contact
+  }
+
+  logging {
+    target_bucket = "${aws_s3_bucket.extract-logs.id}"
+    target_prefix = "log/"
   }
 
   lifecycle_rule {
