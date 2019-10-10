0affa0a02a3827d301ac6eec9d79f09dd74666bf
David Cumps <david@cumps.be>
2019-10-10 13:40:02 +0200

    style: tf format

--- applications/basisregisters/root/dns.tf
+++ applications/basisregisters/root/dns.tf
@@ -16,11 +16,11 @@ module "dns" {
   vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
   public_zone_name = var.alias_zone_name
 
-  api_fqdn    = module.api.api_fqdn
-  docs_fqdn   = module.docs.docs_fqdn
-  wms_db_fqdn = module.wms.wms_fqdn
-  dienstverlening_fqdn = module.publicservice-registry.ui_fqdn
+  api_fqdn                 = module.api.api_fqdn
+  docs_fqdn                = module.docs.docs_fqdn
+  wms_db_fqdn              = module.wms.wms_fqdn
+  dienstverlening_fqdn     = module.publicservice-registry.ui_fqdn
   dienstverlening_api_fqdn = module.publicservice-registry.api_fqdn
-  organisatie_fqdn = module.organisation-registry.ui_fqdn
-  organisatie_api_fqdn = module.organisation-registry.api_fqdn
+  organisatie_fqdn         = module.organisation-registry.ui_fqdn
+  organisatie_api_fqdn     = module.organisation-registry.api_fqdn
 }
