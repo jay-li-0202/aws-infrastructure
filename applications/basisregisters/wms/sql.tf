fea353aecd40327e9575d73c6517d9d29b9c4c95
David Cumps <david@cumps.be>
2019-10-17 17:53:07 +0200

    refactor: dont mind in the sql fails

--- applications/basisregisters/wms/sql.tf
+++ applications/basisregisters/wms/sql.tf
@@ -45,21 +45,26 @@ resource "null_resource" "db_setup" {
 
   provisioner "local-exec" {
     command = "sqlcmd -S tcp:127.0.0.1,10001 -U ${var.sa_user}@${azurerm_sql_server.wms.fully_qualified_domain_name} -P ${var.sa_pass} -d master -q \"${data.template_file.sql1.rendered}\""
+    on_failure = "continue"
   }
 
   provisioner "local-exec" {
     command = "sqlcmd -S tcp:127.0.0.1,10001 -U ${var.sa_user}@${azurerm_sql_server.wms.fully_qualified_domain_name} -P ${var.sa_pass} -d ${var.db_name} -q \"${data.template_file.sql2.rendered}\""
+    on_failure = "continue"
   }
 
   provisioner "local-exec" {
     command = "sqlcmd -S tcp:127.0.0.1,10001 -U ${var.sa_user}@${azurerm_sql_server.wms.fully_qualified_domain_name} -P ${var.sa_pass} -d ${var.db_name} -q \"${data.template_file.sql3.rendered}\""
+    on_failure = "continue"
   }
 
   provisioner "local-exec" {
     command = "sqlcmd -S tcp:127.0.0.1,10001 -U ${var.sa_user}@${azurerm_sql_server.wms.fully_qualified_domain_name} -P ${var.sa_pass} -d master -q \"${data.template_file.sql4.rendered}\""
+    on_failure = "continue"
   }
 
   provisioner "local-exec" {
     command = "sqlcmd -S tcp:127.0.0.1,10001 -U ${var.sa_user}@${azurerm_sql_server.wms.fully_qualified_domain_name} -P ${var.sa_pass} -d ${var.db_name} -q \"${data.template_file.sql5.rendered}\""
+    on_failure = "continue"
   }
 }
